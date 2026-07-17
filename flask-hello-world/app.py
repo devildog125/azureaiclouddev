67dbb64e14b56a0cf5e26a64d6253df02b0114fa
devildog125 <devildog125@gmail.com>
2026-07-16 22:14:42 -0500

    feat: initialize flask application with hello world route

--- flask-hello-world/app.py
+++ flask-hello-world/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "I hate azure TEST ...again"
+    return "hopefully last times"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=True)
