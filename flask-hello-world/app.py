fad9ed2e8fb2691ec6fbc238e976e3be99857953
devildog125 <devildog125@gmail.com>
2026-07-16 22:35:10 -0500

    feat: initialize Flask application with hello world route

--- flask-hello-world/app.py
+++ flask-hello-world/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "another webhook test"
+    return "back again webhook test"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=True)
