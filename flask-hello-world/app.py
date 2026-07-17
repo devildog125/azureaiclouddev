ba25503a4a84ac94f629d487d8ebdcb8b0b9e3b0
devildog125 <devildog125@gmail.com>
2026-07-16 22:05:13 -0500

    feat: initialize flask application with docker configuration

--- flask-hello-world/app.py
+++ flask-hello-world/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "WEB HOOK TEST ...again"
+    return "I hate azure TEST ...again"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=True)
